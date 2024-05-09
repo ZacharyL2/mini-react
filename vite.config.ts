ae423afc50fffefab29686f79dfdb6caf2961693
Zack <islizeqiang@gmail.com>
2024-05-09 11:45:03 +0800

    chore: update project dev config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: [],
+  },
   plugins: [
     react({
       jsxRuntime: 'classic',
-      fastRefresh: false,
     }),
   ],
 });
